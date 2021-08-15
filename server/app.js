650a3699b23c01097c006a9ad9dbd44fe4ee0147
haixiangyan <haixiang6123@gmail.com>
2021-08-16 01:58:20 +0800

    feat: 解决 413 entity too large 的问题

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ app.set('view engine', 'jade');
 
 app.use(cors());
 app.use(logger('dev'));
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
